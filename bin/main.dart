d7c1356aa53a91646b148485e59e76112b056c6c
lazizB17 <abdulazizboriyev993@gmail.com>
2022-04-26 10:43:21 +0500

    comments

--- bin/main.dart
+++ bin/main.dart
@@ -2,6 +2,7 @@ import 'package:http_https_comments/models/comments_model.dart';
 import 'package:http_https_comments/services/http_service.dart';
 
 void main() async {
+  // Comments
   Comments comments = Comments(postId: 12, id: 78, email: "title", body: "body");
   // String response = await NetworkService.POST(NetworkService.apiPosts, NetworkService.headers, post.toJson());
   String response1 = await Http.GET(Http.apiComment + comments.id.toString(), Http.headers);
